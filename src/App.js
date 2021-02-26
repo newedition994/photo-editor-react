05e7df95377a5d27cbe98fd54697d3061ecf1f76
Breyanna Vass <breyannavass@gmail.com>
2021-02-25 21:10:09 -0500

    mapped out the functionality of the app page

--- src/App.js
+++ src/App.js
@@ -75,31 +75,55 @@ const DEFAULT_OPTIONS = [
     },
     unit: 'px'
   }
-
 ]
 
 function App() {
-  return (<div className="container">
-    <div className="main-image" style={getImageStyle()}/>
-    <div className="sidebar">
-      {options.map((option, index) => {
-        return (
-          <SidebarItem 
-          key={index}
-          name={option.name}
-          active={index === selectedOptionIndex}
-          handleClick={() => selectedOptionIndex(index)}
-          />
-        )
-      })}
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
+  const [options, setOptions] = useState(DEFAULT_OPTIONS)
+  const selectedOption = options[selectedOptionIndex]
+
+  function handleSliderChange({ target }) {
+    setOptions(prevOptions => {
+      return prevOptions.map((option, index) => {
+        if (index !== selectedOptionIndex) return option
+        return { ...option, value: target.value }
+      })
+    })
+  }
+
+  function getImageStyle() {
+    const filters = options.map(option => {
+      return `${option.property}(${option.value}${option.unit})`
+    })
+
+    return { filter: filters.join(' ') }
+  }
+
+  console.log(getImageStyle())
+
+  return (
+    <div className="container">
+      <div className="main-image" style={getImageStyle()} />
+      <div className="sidebar">
+        {options.map((option, index) => {
+          return (
+            <SidebarItem
+              key={index}
+              name={option.name}
+              active={index === selectedOptionIndex}
+              handleClick={() => setSelectedOptionIndex(index)}
+            />
+          )
+        })}
+      </div>
+      <Slider
+        min={selectedOption.range.min}
+        max={selectedOption.range.max}
+        value={selectedOption.value}
+        handleChange={handleSliderChange}
+      />
     </div>
-    <Slider 
-      min={selectedOption.range.min}
-      max={selectedOption.range.max}
-      value={selectedOption.value}
-      handleChange={handleSliderChange}
-    />
-  </div>);
+  )
 }
 
 export default App;
