ccc624fd1bf51c5988cd6bbfb15618ffebd8e660
Breyanna Vass <breyannavass@gmail.com>
2021-02-25 21:05:21 -0500

    adding a slider file and sidebar item file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import './App.css';
 
+import Slider from './Slider'
+import SidebarItem from './SidebarItem'
+
 function App() {
-  return null;
+  return (<div className="container">
+    <div className="main-image" style={getImageStyle()}/>
+    <div className="sidebar">
+      {options.map((option, index) => {
+        return (
+          <SidebarItem 
+          key={index}
+          name={option.name}
+          active={index === selectedOptionIndex}
+          handleClick={() => selectedOptionIndex(index)}
+          />
+        )
+      })}
+    </div>
+    <Slider 
+      min={selectedOption.range.min}
+      max={selectedOption.range.max}
+      value={selectedOption.value}
+      handleChange={handleSliderChange}
+    />
+  </div>);
 }
 
 export default App;
